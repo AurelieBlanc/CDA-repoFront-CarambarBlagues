84410875d0b559a057cce839ba0207de5e42f4f6
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-07-16 10:06:46 +0200

    ajout appel fetch et ecouteur event

--- src/components/JokeDisplay/JokeDisplay.jsx
+++ src/components/JokeDisplay/JokeDisplay.jsx
@@ -1,15 +1,44 @@
 // Import des modules nécessaires pour ce composant :  ----------------------//
 import "./JokeDisplay.css"; 
+import { useState } from "react"
+
+
 
 
 
 export default function JokeDisplay() {
+// Déclaration  des states : 
+const [jokeRandomly, setJokeRandomly] = useState(""); 
+
+
+// Fonction qui se déclenchera à chaque fois que l'on cliquera sur le bouton 
+  async function displayJoke() {
+
+
+
+    try {
+
+      const response = await fetch("http://localhost:3000/api/v1/jokes/random")
+
+      if(!response.ok) {
+        throw new Error (`erreur HTTP: ${response.status}`)
+      }
+
+      const data = await response.json(); 
+      console.log("data retour en front", data)
+
+    } catch(error) {
+      console.error("Erreur API : ", error.message)
+    }
+
+  }
+
   return (
     <div class="jokeDisplay">
       
         <div class="containerJD">
-            <button>
-                Teste moi si tu l'oses ! 
+            <button onClick={displayJoke}>
+                Teste-moi si tu l'oses ! 
             </button>
 
             <p>
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,10 @@ import "./Welcome.css"
 export default function Welcome() {
   return (
     <div class="welcome">
-        Bienvenue sur notre page génératrice de blagues incroyables ! Accrochez-vous !
+        <h1>
+          Bienvenue sur notre page génératrice de blagues incroyables ! Accrochez-vous !
+        </h1>
+        
     
     </div>
   )
